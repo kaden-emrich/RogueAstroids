b5339eb82269bc813b2302717e265a69966fec3d
kaden-emrich <125313480+kaden-emrich@users.noreply.github.com>
2023-05-11 10:38:24 -0400

    v0.0.4 (re: last)

--- Main.js
+++ Main.js
@@ -2,7 +2,7 @@ Game.updateSize();
 Game.ctx.strokeStyle = "#fff";
 
 var ship = new Entity("ship" ,Shapes.ship, Angle.degree(0), new Point(100, 100));
-var shipBox = Entity.fromBody(ship.boundingBox, "box");
+var shipBoundingBox = Entity.fromBody(ship.boundingBox, "box");
 //var testEntity = new Entity("test", Shapes.testShape1, Angle.degree(0), new Point(0, 0)); // for debugging
 
 Game.updateInterval = setInterval(function() {
